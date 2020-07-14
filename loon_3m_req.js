456b2ed12eb38c191cbbd8c8b05ca64e66a3d18c
717785320 <40885345+717785320@users.noreply.github.com>
2020-07-14 14:15:29 +0800

    Add files via upload

---
+++ loon_3m_req.js
@@ -0,0 +1,37 @@
+﻿
+console.log("3m_request脚本开始!");
+var url = $request.url;
+var headers = $request.headers;
+var body = $request.body;
+try {
+
+    //打卡时改经纬度
+    if (url.indexOf("/save/m3") > -1) {
+
+        console.log("url:" + url);
+
+        body = JSON.parse(body);
+
+        body.longitude = "114.3196302625787";
+        body.latitude = "30.47188829210171";
+        body.town = "洪山区";
+        body.street = "湖北省武汉市洪山区街道创意天地-06办公楼创意天地";
+        body.nearAddress = "湖北省武汉市洪山区街道创意天地-06办公楼创意天地";
+
+        //body["longitude"] = "114.3196302625787";
+        //body["latitude"] = "30.47188829210171";
+        //body["town"] = "洪山区";
+        //body["street"] = "湖北省武汉市洪山区街道创意天地-06办公楼创意天地";
+        //body["nearAddress"] = "湖北省武汉市洪山区街道创意天地-06办公楼创意天地";
+
+        console.log("body:" + JSON.stringify(body));
+
+        body = JSON.stringify(body);
+    }
+
+} catch (e) {
+    console.log("catch:" + e);
+}
+
+$done({body});
+console.log("执行完成!!!!");
---
+++ loon_jd_sign.js
@@ -0,0 +1,33 @@
+﻿
+console.log("京东签到金豆脚本开始!");
+
+try {
+
+    var Cookie = 'pin=717785320_m;wskey=AAJdrSW2AEB_x8gpN4YY67LMwreL46CJS6AsHwT6V1LuvajnGAaq4RYekVh4qeM9GAM7gfMDf-gsKiv5dwHnEdC_N_7X3GVr;whwswswws=hRTtb5W/D/vXUu2Kx9k7LpAVWIvlMgRPu8ZN+EAl3YMJTPQTSNRT/FC82Mb3kw31Gh1maJx/uu9DgU97mUoQkQA==;unionwsws={"jmafinger":"hRTtb5W\/D\/vXUu2Kx9k7LpAVWIvlMgRPu8ZN+EAl3YMJTPQTSNRT\/FC82Mb3kw31Gh1maJx\/uu9DgU97mUoQkQA==","devicefinger":"eidI3A740111RTI2MjAyRTAtNjMxOC00Rg==S383seL61Kq8IRd1wsJ1jmQZxCvjQ5jy5C5qG\/7luhyvqmrkir+bs0zK4OE\/+g56nSlNx7xkOsxELNC0"}';
+
+    var params = {
+        url: "https://api.m.jd.com/client.action?functionId=signBeanIndex&appid=ld",
+        headers: {
+            Cookie: Cookie
+        }
+    }
+    $httpClient.get(params, function (e, r, d) {
+        console.log("错误:" + e);
+        console.log("返回:" + d);
+        var data = JSON.parse(d);
+        if (data.data.dailyAward.title.indexOf("签到成功") > -1) {
+            $notification.post('京东签到成功!', '京东签到成功', "获得" + data.data.dailyAward.beanAward.beanCount + "个金豆");
+        }
+        else if (data.data.dailyAward.title.indexOf("已签到") > -1) {
+            $notification.post('京东今天已签到!', '京东今天已签到', "获得" + data.data.dailyAward.beanAward.beanCount + "个金豆");
+        }
+        else {
+            $notification.post('京东签到失败!', '京东签到失败', data.data.dailyAward.title);
+        }
+    });
+
+} catch (e) {
+    console.log("catch:" + e);
+}
+$done({});
+console.log("执行完成!!!!");

974f537fb2fad7ba149220bf63de8bb8ef74caa7
717785320 <40885345+717785320@users.noreply.github.com>
2020-07-16 13:24:43 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -3,7 +3,7 @@ console.log("京东签到金豆脚本开始!");
 
 try {
 
-    var Cookie = 'pin=717785320_m;wskey=AAJdrSW2AEB_x8gpN4YY67LMwreL46CJS6AsHwT6V1LuvajnGAaq4RYekVh4qeM9GAM7gfMDf-gsKiv5dwHnEdC_N_7X3GVr;whwswswws=hRTtb5W/D/vXUu2Kx9k7LpAVWIvlMgRPu8ZN+EAl3YMJTPQTSNRT/FC82Mb3kw31Gh1maJx/uu9DgU97mUoQkQA==;unionwsws={"jmafinger":"hRTtb5W\/D\/vXUu2Kx9k7LpAVWIvlMgRPu8ZN+EAl3YMJTPQTSNRT\/FC82Mb3kw31Gh1maJx\/uu9DgU97mUoQkQA==","devicefinger":"eidI3A740111RTI2MjAyRTAtNjMxOC00Rg==S383seL61Kq8IRd1wsJ1jmQZxCvjQ5jy5C5qG\/7luhyvqmrkir+bs0zK4OE\/+g56nSlNx7xkOsxELNC0"}';
+    var Cookie = '';
 
     var params = {
         url: "https://api.m.jd.com/client.action?functionId=signBeanIndex&appid=ld",

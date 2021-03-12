cf3cea9fd010deb060ebed6b7a09d60e2dc9ef3a
717785320 <40885345+717785320@users.noreply.github.com>
2021-03-12 17:14:43 +0800

    Delete test4.js

--- test4.js
+++
@@ -1,3 +0,0 @@
-
-var request = require("request");
-request({url:'http://api.day.app/68K8yA5TgKPpF4kBQQvqcE/' + encodeURI('丸子呦开播了') + '/' + encodeURI('丸子呦开播了') + 'nodejs?sound=calypso'}, function (error, response, body) { });

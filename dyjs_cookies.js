d65570a5d899eb284aa3bd2b17709010f8d7fbe2
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-12-16 17:15:10 +0800

    Update dyjs_cookies.js

--- dyjs_cookies.js
+++ dyjs_cookies.js
@@ -3,12 +3,12 @@ let cookies = [
     {
         "name": "账号1",
         "Cookie": "xxxxxxxxx",
-        "x-Tt-Token": "00c57f3446c92dd48df4a24ae"
+        "x-Tt-Token": "zzzzzzzzzzzzzzzzzzz"
     },
     {
         "name": "账号2",
         "Cookie": "xxxxxxxxx",
-        "x-Tt-Token": "009222c82131597cbe911de23957"
+        "x-Tt-Token": "zzzzzzzzzzzzzzzzzzz"
     }
 
 ];

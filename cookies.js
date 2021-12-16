7b7852df553e0fc41d5abea6e87c4e758e8b9d34
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-12-16 13:44:50 +0800

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,16 @@
+
+let cookies = [
+    {
+        "name": "ук╨е1",
+        "Cookie": "xxxxxxxxx",
+        "x-Tt-Token": "00c57f3446c92dd48df4a24ae"
+    },
+    {
+        "name": "ук╨е2",
+        "Cookie": "xxxxxxxxx",
+        "x-Tt-Token": "009222c82131597cbe911de23957"
+    }
+
+];
+
+module.exports = cookies;

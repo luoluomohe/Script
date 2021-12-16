227dcf3a38601c2acf80e6db9debf2444225e3b1
passerby-b <40885345+passerby-b@users.noreply.github.com>
2021-12-16 13:45:44 +0800

    Update dyjs_new.js

--- dyjs_new.js
+++ dyjs_new.js
@@ -8,21 +8,10 @@
 // [mitm]
 // hostname = *.snssdk.com
 
-//==========================Quantumultx=========================
-// [task_local]
-// */30 * * * * * https://gitee.com/passerby-b/javascript/raw/master/dyjs.js, tag=抖音极速版刷音符, enabled=true
-
-// [rewrite_local]
-// snssdk.com/luckycat/aweme/v1/task/done/read? url script-request-header https://gitee.com/passerby-b/javascript/raw/master/dyjs.js
-
-// =========================Loon=============================
-// [Script]
-// snssdk.com/luckycat/aweme/v1/task/done/read? script-path=https://gitee.com/passerby-b/javascript/raw/master/dyjs.js, requires-body=true, timeout=10, tag=抖音极速版刷音符
-// cron "*/30 * * * * *" script-path=https://gitee.com/passerby-b/javascript/raw/master/dyjs.js,tag=抖音极速版刷音符
-
 //新版APP加密无法抓包,需要降级到14.7.0及以下版本
 
-//0579
+//多账号版,不支持手机
+
 
 let isShowCionDetail = false;//显示收益详情
 
